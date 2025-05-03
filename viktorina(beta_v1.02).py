eff00fe96e4d6d4ee38fc243aff3c558e3ec844f
THEST967 <andrey.p.1243254779@gmail.com>
2025-05-03 22:55:25 +1000

    The new version
    Updated the quiz to 30 questions.

---
+++ viktorina(beta_v1.02).py
@@ -0,0 +1,248 @@
+print(" ██    ██ ██ ██   ██ ████████  ██████  ██████  ██ ███    ██  █████  ")
+print("  ██    ██ ██ ██  ██     ██    ██    ██ ██   ██ ██ ████   ██ ██   ██ ")
+print("  ██    ██ ██ █████      ██    ██    ██ ██████  ██ ██ ██  ██ ███████ ")
+print("  ██  ██  ██ ██  ██     ██    ██    ██ ██   ██ ██ ██  ██ ██ ██   ██ ")
+print("   ████   ██ ██   ██    ██     ██████  ██   ██ ██ ██   ████ ██   ██ ")
+
+print ("viktorina - моя первая тестовая программа на python, программа подразумевает игровой опыть с решением математических примеров. На данный момент игра находится в BETA версии но тем не менее являеться досойным для меня проектом!")
+print ("-----------")
+print ("-----------")
+print ("-----------")
+print ("-----------")
+print("Вопрос 1:")
+print("2 * 8 ?")
+A = int(input())
+if A == 16:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 2:")
+print("5 * 6 ?")
+B = int(input())
+if B == 30:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 3:")
+print("4 * 7 ?")
+C = int(input())
+if C == 28:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 4:")
+print("3 * 9 ?")
+D = int(input())
+if D == 27:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 5:")
+print("6 * 6 ?")
+E = int(input())
+if E == 36:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 6:")
+print("7 * 5 ?")
+F = int(input())
+if F == 35:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 7:")
+print("9 * 2 ?")
+G = int(input())
+if G == 18:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 8:")
+print("8 * 3 ?")
+H = int(input())
+if H == 24:
+    print("Верно!")
+else:
+    print("Неверно!")
+print("Вопрос 9:")
+print("6 * 7 ?")
+H = int(input())
+if I == 42:
+    print("Верно!")
+else:
+    print("Неверно!")
+print ("Вопрос 10:")
+print(" 9 * 55 + 90 - 20 ?" )
+U = int(input())
+if U == 565:
+    print ("Верно!")
+else:
+    print ("Неверно!")
+print ("Вопрос 11:")
+print ("44 + 55 + 66 + 100 - 1 ?")
+A1 == int(inpute())
+if A == 264: 
+    print ("Верно!")
+else:
+    print ("Неверно!")
+print ("Вопрос 12:")
+print("(12 * 12) * 6 + 2 ?")
+A2 = int(input())
+if A2 == 866:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 13:")
+print("15 * 3 ?")
+A3 = int(input())
+if A3 == 45:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 14:")
+print("81 / 9 ?")
+A4 = int(input())
+if A4 == 9:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 15:")
+print("100 - 55 ?")
+A5 = int(input())
+if A5 == 45:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 16:")
+print("11 * 11 ?")
+A6 = int(input())
+if A6 == 121:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 17:")
+print("144 / 12 ?")
+A7 = int(input())
+if A7 == 12:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 18:")
+print("5 * 5 + 10 ?")
+A8 = int(input())
+if A8 == 35:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 19:")
+print("9 * 9 ?")
+A9 = int(input())
+if A9 == 81:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 20:")
+print("7 * 8 ?")
+A10 = int(input())
+if A10 == 56:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 21:")
+print("63 / 7 ?")
+A11 = int(input())
+if A11 == 9:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 22:")
+print("100 / 5 ?")
+A12 = int(input())
+if A12 == 20:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 23:")
+print("45 + 55 ?")
+A13 = int(input())
+if A13 == 100:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 24:")
+print("10 * 10 - 1 ?")
+A14 = int(input())
+if A14 == 99:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 25:")
+print("6 * 12 ?")
+A15 = int(input())
+if A15 == 72:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 26:")
+print("9 * 11 ?")
+A16 = int(input())
+if A16 == 99:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 27:")
+print("8 * 7 ?")
+A17 = int(input())
+if A17 == 56:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 28:")
+print("3 * 15 ?")
+A18 = int(input())
+if A18 == 45:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 29:")
+print("4 * 25 ?")
+A19 = int(input())
+if A19 == 100:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Вопрос 30:")
+print("5 * 9 + 10 ?")
+A20 = int(input())
+if A20 == 55:
+    print("Верно!")
+else:
+    print("Неверно!")
+
+print("Спасибо за игру!")
